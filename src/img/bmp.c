b2bce086e8b8f69aaf1484c5d72de72e4e62519a
Marek Maskarinec <fazolovy.kotlik@gmail.com>
2021-02-21 21:34:47 +0100

    some changes to bmp parser (still doesn't work)

--- src/img/bmp.c
+++ src/img/bmp.c
@@ -7,25 +7,29 @@
 int peekn(FILE *file, unsigned int n) {
 	int tr = 0;
 
-	for (int i=0; i < n; i++) {
+	for (unsigned int i=0; i < n; i++) {
 		tr += (fgetc(file) << (8 * i));
 	}
 
 	return tr;
 }
 
-void parsebmp(char *path, img_t *img) {
+void parsebmp(char path[], img_t *img) {
 	int cb;
 	long int c;
 
-	printf("%d", *path);
+	printf("%d\n", path[0]);
 
-	return;
-
-	FILE *f = fopen(path, "r");
+	FILE *f = fopen(&path[0], "r");
+	if (f == NULL) {
+		printf("could not open file\n");
+		return;
+	}
 
 	while ((cb = fgetc(f)) != EOF) {
 		c = ftell(f);
+
+		printf("current byte: %d\n", c);
 
 		switch (c) {
 		case 3:
@@ -47,15 +51,25 @@ void parsebmp(char *path, img_t *img) {
 			img->bitcount = cb + peekn(f, 1);
 			break;
 		case 35:
+			printf("found sizeimage byte\n");
 			img->sizeimage = cb + peekn(f, 3);
 			break;
 		default:
+
+			if (img->sizeimage < 1) {
+				break;
+			}
+
+			printf("starting img parsing\n");			
+
 			img->content = malloc(img->sizeimage * sizeof(uint32_t));
 
 			img->content[0] = cb;
+	
+			printf("%d\n", img->sizeimage);
 
-			for (int i=0; i < img->sizeimage; i++) {
-				img->content[i] = (uint32_t)fgetc(f);
+			for (int i=1; i < img->sizeimage && (cb = fgetc(f)) != EOF; i++) {
+				img->content[i] = (uint32_t)cb;
 			}
 			return;
 			break;
