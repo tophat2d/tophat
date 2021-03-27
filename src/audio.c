52e30abdb5337ffc601c53436138a71c1c5cd21a
Marek Maskarinec <fazolovy.kotlik@gmail.com>
2021-03-27 09:23:30 +0100

    cleanup and lower the binary size

--- src/audio.c
+++ src/audio.c
@@ -3,6 +3,10 @@
 // inpired by https://github.com/drmargarido's sound module
 #include <stdint.h>
 
+#define MA_NO_FLAC
+#define MA_NO_ENCODING
+#define MA_NO_COREAUDIO
+#define MA_NO_WEBAUDIO
 #define MINIAUDIO_IMPLEMENTATION
 #include "../lib/miniaudio/miniaudio.h"
 
