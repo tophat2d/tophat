e0d1c3537c2c1d2feb4c6171412324ce81b9c347
Marek Maskarinec <marekmaskarinec@p>
2021-05-25 11:39:07 +0200

    fix tilemap collision

--- src/collisions.h
+++ src/collisions.h
@@ -3,11 +3,12 @@
 
 #include "polygon.h"
 #include "tilemap.h"
+#include <stdbool.h>
 
 int _th_poly_to_poly(th_poly *p1, th_poly *p2, int *ix, int *iy);
 int _th_poly_to_line(th_poly *a, int sx, int sy, int ex, int ey, int *ix, int *iy);
 int _th_line_to_line(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int *ix, int *iy);
 int _th_poly_to_point(th_poly *a, int px, int py, int *ix, int *iy);
-int _th_coll_on_tilemap(th_poly *p, th_tmap *t, int *rx, int *ry);
+bool _th_coll_on_tilemap(th_poly *p, th_tmap *t, int *rx, int *ry);
 
 #endif
