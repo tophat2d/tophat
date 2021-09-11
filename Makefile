tophat2d/tophat

commit ff6ec65cd4a0bf7eaf6ca31338dabce883cd1533

store cpoly in cent by value

src/entity.c
@@ -14,36 +14,33 @@ void th_ent_draw(th_ent *o, th_rect *camera) {
 	camx = camera->x - (camera->w / 2);
 	camy = camera->y - (camera->h / 2);
 
-	if (o->p->x + o->p->w < camx || o->p->x - o->p->w > camx + camera->w)
+	if (o->p.x + o->p.w < camx || o->p.x - o->p.w > camx + camera->w)
 		return;
 
-	if (o->p->y + o->p->h < camy || o->p->y - o->p->h > camy + camera->h)
+	if (o->p.y + o->p.h < camy || o->p.y - o->p.h > camy + camera->h)
 		return;
 
 	if (o->img == NULL) {
-		if (o->p == NULL)
-			th_error("invalid entity with id %d", o->id);
-
 		RDPoint *points;
 
-		points = _th_poly_to_rdpoint(o->p, camx, camy);
+		points = _th_poly_to_rdpoint(&o->p, camx, camy);
 		CNFGColor(o->color);
-		CNFGTackPoly(points, o->p->vc);
+		CNFGTackPoly(points, o->p.vc);
 		free(points);
 
 		return;
 	}
 
-	th_blit_tex(o->img->tex, (o->p->x-camx) * scaling, (o->p->y-camy) * scaling, o->img->w * o->sx * scaling, o->img->h * o->sy * scaling, o->rot);
+	th_blit_tex(o->img->tex, (o->p.x-camx) * scaling, (o->p.y-camy) * scaling, o->img->w * o->sx * scaling, o->img->h * o->sy * scaling, o->rot);
 }
 
-int th_ent_getcoll(th_ent *e, th_ent **scene, int count, int *ix, int *iy) {
+int th_ent_getcoll(th_ent *e, th_ent *scene, int count, int *ix, int *iy) {
 	int coll;
 
-	int ex = e->p->x;
-	int ey = e->p->y;
-	int ew = e->p->w;
-	int eh = e->p->h;
+	int ex = e->p.x;
+	int ey = e->p.y;
+	int ew = e->p.w;
+	int eh = e->p.h;
 	
 	if (!ex || !ey)
 		return 0;
@@ -54,12 +51,12 @@ int th_ent_getcoll(th_ent *e, th_ent **scene, int count, int *ix, int *iy) {
 		eh = abs(eh);
 
 	for (int i=0; i < count; i++) {
-		if (e->id == scene[i]->id) continue;
+		if (e->id == scene[i].id) continue;
 
-		int sx = e->p->x;
-		int sy = e->p->y;
-		int sw = e->p->w;
-		int sh = e->p->h;
+		int sx = e->p.x;
+		int sy = e->p.y;
+		int sw = e->p.w;
+		int sh = e->p.h;
 		
 		if (sw < 0)
 			sw = abs(sw);
@@ -71,14 +68,14 @@ int th_ent_getcoll(th_ent *e, th_ent **scene, int count, int *ix, int *iy) {
 		if (ew + ex < sx) continue;
 		if (eh + ey < sy) continue;
 
-		coll = _th_poly_to_poly(scene[i]->p, e->p, ix, iy);
+		coll = _th_poly_to_poly(&scene[i].p, &e->p, ix, iy);
 		if (coll) {
-			return scene[i]->id;
+			return scene[i].id;
 		}
 
-		coll = _th_poly_to_poly(e->p, scene[i]->p, ix, iy);
+		coll = _th_poly_to_poly(&e->p, &scene[i].p, ix, iy);
 		if (coll) {
-			return scene[i]->id;
+			return scene[i].id;
 		}
 	}
 	return 0;

umka/polygon.um
@@ -32,6 +32,17 @@ type cpoly* = struct {
 	vc: int32
 }
 
+// Internal function converting p to c compatible polygon
+fn (p: ^poly) toc*(): cpoly {
+	if len(p.v) == 0 {
+		return cpoly{}
+	}
+
+	return cpoly{
+		round(p.x), round(p.y),
+		&p.v[0], p.w, p.h, len(p.v) * 2}
+}
+
 // Makes a polygon. *USE* this.
 fn mk*(x, y: real, v: []int32): poly {
 	var p: poly

makefile
@@ -37,7 +37,9 @@ install: build
 wbuild: libs
 	$(MINGW) -o tophat.exe $(SRC) $(WIN_FULL) $(RELEASE_FLAGS) -s -Os
 
-run: build
+run:
+	rm src/bindings.o
+	make build
 	./tophat
 
 clean:

src/bindings.c
@@ -197,7 +197,7 @@ void umtmapgetcoll(UmkaStackSlot *p, UmkaStackSlot *r) {
 	int *ty = (int *)p[4].ptrVal;
 	int *tx = (int *)p[5].ptrVal;
 
-	r->intVal = _th_coll_on_tilemap(ent->p, t, x, y, tx, ty);
+	r->intVal = _th_coll_on_tilemap(&ent->p, t, x, y, tx, ty);
 }
 
 ///////////////////////
@@ -335,7 +335,7 @@ void umentdraw(UmkaStackSlot *p, UmkaStackSlot *r) {
 }
 
 void umentgetcoll(UmkaStackSlot *p, UmkaStackSlot *r) {
-	th_ent **scene = (th_ent **)p[0].ptrVal;
+	th_ent *scene = (th_ent *)p[0].ptrVal;
 	th_ent *e = (th_ent *)p[1].ptrVal;
 	int count = p[2].intVal;
 	int *iy = (int *)p[3].ptrVal;
@@ -408,7 +408,7 @@ void umsoundvalidate(UmkaStackSlot *p, UmkaStackSlot *r) {
 ///////////////////////
 // raycast
 void umraygetcoll(UmkaStackSlot *p, UmkaStackSlot *r) {
-	th_ent **scene = (th_ent **)p[0].ptrVal;
+	th_ent *scene = (th_ent *)p[0].ptrVal;
 	th_ray *ra = (th_ray *)p[1].ptrVal;
 	int count = p[2].intVal;
 	int *iy = (int *)p[3].ptrVal;

src/entity.h
@@ -6,8 +6,9 @@
 #include "polygon.h"
 #include "image.h"
 
+#pragma pack(push, 1)
 typedef struct {
-	th_poly *p;
+	th_poly p;
 	th_image *img;
 	double sx;
 	double sy;
@@ -15,8 +16,9 @@ typedef struct {
 	uint32_t color;
 	int id;
 } th_ent;
+#pragma pack(pop)
 
 void th_ent_draw(th_ent *o, th_rect *camera);
-int th_ent_getcoll(th_ent *e, th_ent **scene, int count, int *ix, int *iy);
+int th_ent_getcoll(th_ent *e, th_ent *scene, int count, int *ix, int *iy);
 
 #endif

src/polygon.h
@@ -3,6 +3,7 @@
 
 #include "../lib/rawdraw/CNFG.h"
 
+#pragma pack(push, 1)
 typedef struct {
 	int x;
 	int y;
@@ -11,6 +12,7 @@ typedef struct {
 	int h;
 	int vc;
 } th_poly;
+#pragma pack(pop)
 
 void th_new_poly(th_poly *p, int px, int py, int vertnum, ...);
 void th_free_poly(th_poly *p);

commit 68a1b808e000c51ac5cf92345e84111d084d2de6

Rework ui.um a bit
	!!! This change isn't backwards compatible !!!
	In the new system, there is a generic struct
	that all elements have. Element now also requires
	get function returning a pointer to element's generic
	struct. This replaces gspan, gpadding and gdimensions.

Fix UB with polygons

	This bug was introduced in ff6ec65cd4a0bf7eaf6ca31338dabce883cd1533.
	It was very stupid - I just wrote * instead of /. This meant the
	vertice count passed to c was 4 times bigger than it actually was.

fix space shooter

Makefile
@@ -21,14 +21,14 @@ CFLAGS += -Ilib/stb/ \
 		  -Ilib/rawdraw/ \
 		  -Ilib/miniaudio/
 
-DEFS += -DCNFGOGL -DUMKA_STATIC
+DEFS += -DCNFGOGL -DUMKA_STATIC -DRELEASE_BUILD
 WARNS = \
 		-Wall -Wextra -Wno-unused-parameter -Wno-sign-compare \
 		-Wno-old-style-declaration -Wno-implicit-fallthrough
 
 CFLAGS += $(WARNS) $(DEFS)
 
-all: $(TARGET)
+all: $(UMKA_LIB) $(TARGET)
 
 run: $(TARGET)
 	./$(TARGET)

cmd/embedder.um
@@ -75,7 +75,7 @@ fn strtoc(inp: str): str {
 }
 
 fn main() {
-	printf("#ifndef UMKALIBS_H\n#define UMKALIBS_H\nconst char *libs[] = {\n")
+	printf("const char *libs[] = {\n")
 
 	for i:=1; i < std.argc(); i++ {
 		f := std.fopen(std.argv(i), "r")
@@ -84,5 +84,5 @@ fn main() {
 		std.fclose(f)
 	}
 
-	printf("};\n#endif\n")
+	printf("};\n")
 }

examples/space-shooter/bullet.um
@@ -1,9 +1,9 @@
 
 import (
-	"tophat/entity.um"
-	"tophat/image.um"
-	"tophat/polygon.um"
-	"tophat/tophat.um"
+	"entity.um"
+	"image.um"
+	"polygon.um"
+	"tophat.um"
 
 	"enemy.um"
 	"global.um"
@@ -27,7 +27,6 @@ fn handle*(scn: []^entity.ent) {
 
 	// goes through all bullets
 	for i:=0; i < len(all); i++ {
-
 		// checks, if they are above the screen, so we don't end up with a lot of them
 		if all[i].p.y < global.cam.y-global.cam.h {
 			all = delete(all, i)

examples/space-shooter/deathscn.um
@@ -1,9 +1,9 @@
 
 import (
-	"tophat/ui.um"
-	"tophat/rectangle.um"
+	"ui.um"
+	"rectangle.um"
 
-	"tophat/std/std.um"
+	"std.um"
 
 	"global.um"
 	"gamescn.um"
@@ -36,17 +36,17 @@ fn menupress() {
 fn init*() {
 	retrybutton = ui.mkbutton(ui.mklabel("RETRY", 1, 0x22ff))
 	retrybutton.label.bgcolor = 0xeeeeeeff
-	retrybutton.padding = rectangle.mk(10, 10, 10, 10)
+	retrybutton.g.padding = rectangle.mk(10, 10, 10, 10)
 	retrybutton.onrelease = retrypress
 
 	menubutton = ui.mkbutton(ui.mklabel("TO MENU", 1, 0x22ff))
 	menubutton.label.bgcolor = 0xeeeeeeff
-	menubutton.padding = rectangle.mk(10, 10, 10, 10)
+	menubutton.g.padding = rectangle.mk(10, 10, 10, 10)
 	menubutton.onrelease = menupress
 
 	mainbox = ui.mkbox([]ui.element{ui.mkbox([]ui.element{ui.mklabel("GAME OVER", 2, 0xeeeeeeff), ui.mklabel("SCORE " + repr(global.score), 1, 0xeeeeeeff)}, false),
 	 &retrybutton, &menubutton}, false)
-	mainbox.padding = rectangle.mk(10, 10, 10, 10)
+	mainbox.g.padding = rectangle.mk(10, 10, 10, 10)
 
 	ui.mainelement = mainbox
 }

examples/space-shooter/effects.um
@@ -1,11 +1,11 @@
 
 import (
-	"tophat/tophat.um"
-	"tophat/rawdraw.um"
-	"tophat/rectangle.um"
-	"tophat/animation.um"
+	"tophat.um"
+	"rawdraw.um"
+	"rectangle.um"
+	"animation.um"
 
-	"tophat/std/std.um"
+	"std.um"
 )
 
 // type used for star

examples/space-shooter/enemy.um
@@ -1,17 +1,17 @@
 
 import (
-	"tophat/entity.um"
-	"tophat/rectangle.um"
-	"tophat/polygon.um"
-	"tophat/image.um"
-	"tophat/input.um"
-	"tophat/tophat.um"
-	"tophat/rawdraw.um"
-	"tophat/animation.um"
-	"tophat/tophat.um"
-	"tophat/audio.um"
+	"entity.um"
+	"rectangle.um"
+	"polygon.um"
+	"image.um"
+	"input.um"
+	"tophat.um"
+	"rawdraw.um"
+	"animation.um"
+	"tophat.um"
+	"audio.um"
 
-	"tophat/std/std.um"
+	"std.um"
 
 	"global.um"
 	"settings.um"

examples/space-shooter/gamescn.um
@@ -1,14 +1,14 @@
 
 import (
-	"tophat/entity.um"
-	"tophat/rectangle.um"
-	"tophat/polygon.um"
-	"tophat/image.um"
-	"tophat/input.um"
-	"tophat/tophat.um"
-	"tophat/rawdraw.um"
+	"entity.um"
+	"rectangle.um"
+	"polygon.um"
+	"image.um"
+	"input.um"
+	"tophat.um"
+	"rawdraw.um"
 
-	"tophat/std/std.um"
+	"std.um"
 
 	"enemy.um"
 	"player.um"

examples/space-shooter/global.um
@@ -1,7 +1,7 @@
 
 import (
-	"tophat/rectangle.um"
-	"tophat/tophat.um"
+	"rectangle.um"
+	"tophat.um"
 
 	"effects.um" // global shouldn't import anything else, than tophat package. TODO
 )

examples/space-shooter/main.um
@@ -16,10 +16,10 @@
 
 
 import (
-	"tophat/rectangle.um"
-	"tophat/tophat.um"
-	"tophat/rawdraw.um"
-	"tophat/input.um"
+	"rectangle.um"
+	"tophat.um"
+	"rawdraw.um"
+	"input.um"
 
 	"gamescn.um"
 	"deathscn.um"

examples/space-shooter/menu.um
@@ -1,7 +1,7 @@
 
 import (
-	"tophat/ui.um"
-	"tophat/rectangle.um"
+	"ui.um"
+	"rectangle.um"
 
 	"gamescn.um"
 	"global.um"
@@ -17,14 +17,14 @@ var (
 fn init*() {
 	playbutton = ui.mkbutton(ui.mklabel("PLAY", 1, 0x22ff))
 	playbutton.label.bgcolor = 0xeeeeeeff
-	playbutton.padding = rectangle.mk(10, 10, 10, 10)
+	playbutton.g.padding = rectangle.mk(10, 10, 10, 10)
 
 	settingsbutton = ui.mkbutton(ui.mklabel("QUIT", 1, 0x22ff))
 	settingsbutton.label.bgcolor = 0xeeeeeeff
-	settingsbutton.padding = rectangle.mk(10, 10, 10, 10)
+	settingsbutton.g.padding = rectangle.mk(10, 10, 10, 10)
 
 	mainbox = ui.mkbox([]ui.element{ui.mklabel("SPACE GAME", 2, 0xeeeeeeff), &playbutton, &settingsbutton}, false)
-	mainbox.padding = rectangle.mk(10, 20, 10, 20)
+	mainbox.g.padding = rectangle.mk(10, 20, 10, 20)
 
 	ui.mainelement = mainbox
 }

examples/space-shooter/player.um
@@ -1,10 +1,10 @@
 
 import (
-	"tophat/entity.um"
-	"tophat/image.um"
-	"tophat/polygon.um"
-	"tophat/input.um"
-	"tophat/tophat.um"
+	"entity.um"
+	"image.um"
+	"polygon.um"
+	"input.um"
+	"tophat.um"
 //	"tophat/audio.um"
 
 	"bullet.um"

examples/space-shooter/sounds.um
@@ -1,6 +1,6 @@
 
 import (
-	"tophat/audio.um"
+	"audio.um"
 )
 
 var (

main.um
@@ -19,48 +19,21 @@ import (
 )
 
 fn main() { 
-	cam := rectangle.mk(0, 0, 250, 250)
+	cam := rectangle.mk(20, 20, 250, 250)
 	tophat.setup("test game", 100, 100)
 	var w, h: int32
 
 	t := 0
 
-	m := light.mk(0xff, 0.1, cam)
-
-	spot := light.spotlight{0, 0, 20, 0xff000000}
-	spot2 := light.spotlight{0, 0, 40, 0xffff0000}
-
-	img := image.load("etc/test.png")
+	p := polygon.mk(100, 100, []int32{0, 0, 5, 0, 5, 5, 0, 5})
+	e := entity.mk(p, tophat.black, 0)
 
 	for true {
 		tophat.cycle(&w, &h, 0xffffffff, cam)
 		t += tophat.delta
 
-		m.clear()
-
-		img.draw(0, 0, 0, 10)
-
-		spot.x, spot.y = input.getglobalmousepos(cam)
-
-		spot.stamp(&m, cam)
-		spot2.stamp(&m, cam)
-		m.draw(cam)
-
-		if input.ispressed(int32('a')) {
-			cam.x += round(0.1 * tophat.delta)
-		}
-
-		if input.ispressed(int32('d')) {
-			cam.x -= round(0.1 * tophat.delta)
-		}
-
-		if input.ispressed(int32('w')) {
-			cam.y += round(0.1 * tophat.delta)
-		}
-
-		if input.ispressed(int32('s')) {
-			cam.y -= round(0.1 * tophat.delta)
-		}
+		p.draw(0xaa)
+		e.draw(cam)
 
 		if tophat.delta != 0 {
 			rawdraw.drawtext(repr(1000/tophat.delta), 0, 0, tophat.white, 2)

src/Makefile
@@ -9,16 +9,17 @@ DEPS = $(OBJS:.o=.d)
 
 UMKA_LIB = src/umkalibs.h
 
-$(UMKA_LIB): $(UMKA_LIBRARY)
+umkalibs: $(UMKA_LIBRARY)
 	@echo EM $@
-	@./lib/umka/build/umka cmd/embedder.um umka/animation.um umka/audio.um umka/csv.um umka/entity.um umka/image.um umka/input.um umka/misc.um umka/polygon.um umka/rawdraw.um umka/raycast.um umka/rectangle.um umka/tilemap.um umka/tophat.um umka/ui.um umka/vec.um umka/std/std.um umka/std/std.um >> src/umkalibs.h
+	@./lib/umka/build/umka cmd/embedder.um umka/animation.um umka/audio.um umka/csv.um umka/entity.um umka/image.um umka/input.um umka/misc.um umka/polygon.um umka/rawdraw.um umka/raycast.um umka/rectangle.um umka/tilemap.um umka/tophat.um umka/ui.um umka/vec.um umka/std/std.um umka/std/std.um > src/umkalibs.h
+	rm -f src/bindings.o
 
-%.o: %.c $(UMKA_LIB)
+%.o: %.c
 	@echo CC $@
 	@$(CC) $(CFLAGS) -MM -MG -MF $(patsubst %.o,%.d,$@) -c $<
 	@$(CC) $(CFLAGS) -o $@ -c $<
 
-$(TARGET): $(OBJS) $(UMKA_LIBRARY)
+$(TARGET): umkalibs $(OBJS) $(UMKA_LIBRARY)
 	@echo LD $@
 	$(CC) $(CFLAGS) -o $@ $(OBJS) $(LDLIBS) $(LDFLAGS)
 

src/entity.c
@@ -28,12 +28,13 @@ void th_ent_draw(th_ent *o, th_rect *camera) {
 		return;
 	}
 
-	th_blit_tex(o->img->tex,
-	           (o->p.x-camx) * scaling,
-	           (o->p.y-camy) * scaling,
-						  o->img->w * o->sx * scaling,
-							o->img->h * o->sy * scaling,
-							o->rot);
+	th_blit_tex(
+		o->img->tex,
+	  (o->p.x-camx) * scaling,
+	  (o->p.y-camy) * scaling,
+		o->img->w * o->sx * scaling,
+		o->img->h * o->sy * scaling,
+		o->rot);
 }
 
 int th_ent_getcoll(th_ent *e, th_ent *scene, int count, int *ix, int *iy) {

src/polygon.c
@@ -6,8 +6,9 @@
 
 extern float scaling;
 
+// TODO: don't do this on the heap
 RDPoint *_th_poly_to_rdpoint(th_poly *p, int camx, int camy) {
-	RDPoint *tr = malloc(sizeof(int) * p->vc * 2);
+	RDPoint *tr = malloc(sizeof(RDPoint) * p->vc);
 
 	for (int i=0; i < p->vc * 2; i += 2) {
 		tr[i/2].x = (p->x + p->v[i] - camx) * scaling;

umka/polygon.um
@@ -37,7 +37,7 @@ fn (p: ^poly) toc*(): cpoly {
 
 	return cpoly{
 		round(p.x), round(p.y),
-		&p.v[0], p.w, p.h, len(p.v) * 2}
+		&p.v[0], p.w, p.h, len(p.v)/2}
 }
 
 // Makes a polygon. USE this.

umka/ui.um
@@ -1,5 +1,5 @@
-// Small and modulat ui module useful for stuff like menus.
-// Set any element as ui.mainelement and it will be the root.
+// Small and modular ui module useful for stuff like menus.
+// Set ui.mainelement to your main element.
 
 import (
 	"rectangle.um"
@@ -11,24 +11,32 @@ import (
 
 const (
 	// text aligns
-	TOP* = 0
-	MID* = 1
-	BOT* = 2
+	MID* = 0
+	TOP*
+	BOT*
 
 	// image modes
-	CENTER* = 3
-	STRETCH* = 4
-	SCALE* = 5
+	CENTER* = 0
+	STRETCH*
+	SCALE*
 )
+
+// These are properties every element should have.
+type generic* = struct {
+	padding: rectangle.rect // amount of space removed from the space available
+	
+	// position and dimensions. You can set them manually, or use box for automatic
+	// placement.
+	dimensions: rectangle.rect
+	span: int // how many elements in a box it spans
+}
 
 // Elements is the interface for all ui elements. You can make your own elements.
 type element* = interface {
 	draw() // draws the element
 	calculate() // calculates it's dimensions
 	handle() // handle cycle
-	gspan(): ^int // returns pointer to it's span
-	gpadding(): ^rectangle.rect // returns pointer to it's padding
-	gdimensions(): ^rectangle.rect // returns pointer to it's dimensions
+	get(): ^generic // returns a pointer to it's generic
 }
 
 // Box can be used to oranize
@@ -37,9 +45,8 @@ type element* = interface {
 // It has children, which are organized
 // on it's calculate call.
 type box* = struct {
+	g: generic
 	children: []element
-	padding, dimensions: rectangle.rect
-	span: int // how many elements in box it spans
 	vertical: bool // false: hbox
 
 	image: image.img
@@ -50,14 +57,13 @@ type box* = struct {
 
 // Label can be used to draw text.
 type label* = struct {
+	g: generic
 	text: str
 	textsize: real
 	textcolor: uint32
 	valign: int // alignment
 	halign: int
 	bgcolor: uint32
-	padding, dimensions: rectangle.rect
-	span: int
 }
 
 // Button checks for clicks on it's
@@ -70,8 +76,7 @@ type button* = struct {
 	justpressed: bool // is just pressed
 	justreleased: bool // is just release
 
-	padding, dimensions: rectangle.rect
-	span: int
+	g: generic
 
 	onpress, onjustpress, onrelease: fn() // callbacks
 	ontoggle: fn(toggled: bool)
@@ -83,10 +88,11 @@ type button* = struct {
 fn mkbox*(c: []element, vertical: bool): box {
 	h := box {}
 
-	h.padding = rectangle.mk(0, 0, 0, 0)
+	h.g = generic{
+		padding: rectangle.rect{},
+		dimensions: rectangle.rect{},
+		span: 1}
 	h.children = c
-	h.dimensions = rectangle.mk(0, 0, 0, 0)
-	h.span = 1
 	h.vertical = vertical
 	h.imagemode = CENTER
 
@@ -94,43 +100,45 @@ fn mkbox*(c: []element, vertical: bool): box {
 }
 
 fn (b: ^box) calculate*() {
+	generics := make([]^generic, len(b.children))
+	for i:=0; i < len(b.children); i++ {
+		generics[i] = b.children[i].get()
+	}
 
 	totalspan := 0.0
 	off := 0
 
 	for i:=0; i < len(b.children); i++ {
-		totalspan += b.children[i].gspan()^
+		totalspan += generics[i].span
 	}
 
 	for i:=0; i < len(b.children); i++ {
+		dims := &generics[i].dimensions
+		padd := &generics[i].padding
 
 		if b.vertical {
-			dims := b.children[i].gdimensions()
-			padd := b.children[i].gpadding()
       
-			dims.x = b.dimensions.x + off + padd.x
-			dims.w = round(real(b.dimensions.w)/totalspan) * b.children[i].gspan()^ - padd.w
+			dims.x = b.g.dimensions.x + off + padd.x
+			dims.w = round(real(b.g.dimensions.w)/totalspan) * generics[i].span - padd.w
       
-			dims.y = b.dimensions.y + padd.y
-			dims.h = b.dimensions.h - padd.h
+			dims.y = b.g.dimensions.y + padd.y
+			dims.h = b.g.dimensions.h - padd.h
       
 			b.children[i].calculate()
       
-			off += round(real(b.dimensions.w)/totalspan) * b.children[i].gspan()^
+			off += round(real(b.g.dimensions.w)/totalspan) * generics[i].span
 		
 		} else {
-			dims := b.children[i].gdimensions()
-			padd := b.children[i].gpadding()
+			dims.x = b.g.dimensions.x + padd.x
+			dims.w = b.g.dimensions.w - padd.w - padd.x
       
-			dims.x = b.dimensions.x + padd.x
-			dims.w = b.dimensions.w - padd.w - padd.x
-      
-			dims.y = b.dimensions.y + off + padd.y
-			dims.h = round(real(b.dimensions.h)/totalspan * b.children[i].gspan()^) - padd.h - padd.y
+			dims.y = b.g.dimensions.y + off + padd.y
+			dims.h = round(real(b.g.dimensions.h)/totalspan * generics[i].span) -
+				padd.h - padd.y
       
 			b.children[i].calculate()
       
-			off += round(real(b.dimensions.h)/totalspan) * b.children[i].gspan()^
+			off += round(real(b.g.dimensions.h)/totalspan) * generics[i].span
 		}
 	}
 
@@ -144,13 +152,13 @@ fn (b: ^box) calculate*() {
 
 		w, h := b.image.getdims()
 
-		b.imgx = b.dimensions.x + (b.dimensions.w-w)/2
-		b.imgy = b.dimensions.y + (b.dimensions.h-h)/2
+		b.imgx = b.g.dimensions.x + (b.g.dimensions.w-w)/2
+		b.imgy = b.g.dimensions.y + (b.g.dimensions.h-h)/2
 	case SCALE:
 		w, h := b.image.getdims()
 
-		sx := b.dimensions.w / w
-		sy := b.dimensions.h / h
+		sx := b.g.dimensions.w / w
+		sy := b.g.dimensions.h / h
 
 		b.imgs = sy
 
@@ -158,8 +166,8 @@ fn (b: ^box) calculate*() {
 			b.imgs = sx
 		}
 
-		b.imgx = b.dimensions.x + (b.dimensions.w - w*b.imgs)/2
-		b.imgy = b.dimensions.y + (b.dimensions.h - h*b.imgs)/2
+		b.imgx = b.g.dimensions.x + (b.g.dimensions.w - w*b.imgs)/2
+		b.imgy = b.g.dimensions.y + (b.g.dimensions.h - h*b.imgs)/2
 	}
 }
 
@@ -174,16 +182,8 @@ fn (hb: ^box) draw*() {
 	}
 }
 
-fn (hb: ^box) gspan*(): ^int {
-	return &hb.span
-}
-
-fn (hb: ^box) gpadding*(): ^rectangle.rect {
-	return &hb.padding
-}
-
-fn (hb: ^box) gdimensions*(): ^rectangle.rect {
-	return &hb.dimensions
+fn (b: ^box) get*(): ^generic {
+	return &b.g
 }
 
 fn (hb: ^box) handle*() {
@@ -198,13 +198,15 @@ fn (hb: ^box) handle*() {
 fn mklabel*(text: str, textsize: real, textcolor: uint32): label {
 	h := label {}
 
+	h.g = generic{
+		padding: rectangle.rect{},
+		dimensions: rectangle.rect{},
+		span: 1}
+
 	h.text = text
 	h.textsize = textsize
 	h.textcolor = textcolor
 	h.bgcolor = 0x0
-	h.padding = rectangle.mk(0, 0, 0, 0)
-	h.dimensions = rectangle.mk(0, 0, 0, 0)
-	h.span = 1
 	h.valign = MID
 	h.halign = MID
 
@@ -216,36 +218,33 @@ fn (l: ^label) calculate*() {
 
 	switch l.valign {
 	case MID:
-		l.padding.y = (l.dimensions.h-sy)/2
+		l.g.padding.y = (l.g.dimensions.h-sy)/2
 	case BOT:
-		l.padding.y = l.dimensions.h - sy
+		l.g.padding.y = l.g.dimensions.h - sy
 	}
 
 	switch l.halign {
 	case MID:
-		l.padding.x = (l.dimensions.w-sx)/2
+		l.g.padding.x = (l.g.dimensions.w-sx)/2
 	case BOT:
-		l.padding.x = l.dimensions.w - sx
+		l.g.padding.x = l.g.dimensions.w - sx
 	}
 }
 
 fn (l: ^label) draw*() {
 	rawdraw.setcolor(l.bgcolor)
-	rawdraw.drawrect(l.dimensions.x - l.padding.x, l.dimensions.y - l.padding.y, l.dimensions.w + l.padding.w, l.dimensions.h + l.padding.h)
+	rawdraw.drawrect(
+		l.g.dimensions.x - l.g.padding.x,
+		l.g.dimensions.y - l.g.padding.y,
+		l.g.dimensions.w + l.g.padding.w,
+		l.g.dimensions.h + l.g.padding.h)
 
-	rawdraw.drawtext(l.text, l.dimensions.x, l.dimensions.y, l.textcolor, l.textsize)
+	rawdraw.drawtext(
+		l.text, l.g.dimensions.x, l.g.dimensions.y, l.textcolor, l.textsize)
 }
 
-fn (l: ^label) gspan*(): ^int {
-	return &l.span
-}
-
-fn (l: ^label) gpadding*(): ^rectangle.rect {
-	return &l.padding
-}
-
-fn (l: ^label) gdimensions*(): ^rectangle.rect {
-	return &l.dimensions
+fn (l: ^label) get*(): ^generic {
+	return &l.g
 }
 
 fn (l: ^label) handle*() {}
@@ -256,12 +255,15 @@ fn (l: ^label) handle*() {}
 fn mkbutton*(l: label): button {
 	b := button{}
 
-	b.padding = l.padding
+	b.g = generic{
+		padding: l.g.padding,
+		dimensions: rectangle.rect{},
+		span: 1}
+
 	b.label = l
 	b.pressed = false
 	b.justpressed = false
 	b.toggled = false
-	b.span = 1
 
 	b.onpress = fn() {}
 	b.onjustpress = fn() {}
@@ -272,38 +274,32 @@ fn mkbutton*(l: label): button {
 }
 
 fn (b: ^button) calculate*() {
-	b.label.dimensions = b.dimensions
+	b.label.g.dimensions = b.g.dimensions
 	b.label.calculate()
-	b.label.dimensions.x += b.label.padding.x
-	b.label.dimensions.w -= b.label.padding.w
+	b.label.g.dimensions.x += b.label.g.padding.x
+	b.label.g.dimensions.w -= b.label.g.padding.w
 
-	b.label.dimensions.y += b.label.padding.y
-	b.label.dimensions.h -= b.label.padding.h
+	b.label.g.dimensions.y += b.label.g.padding.y
+	b.label.g.dimensions.h -= b.label.g.padding.h
 }
 
 fn (b: ^button) draw*() {
 	b.label.draw()
 }
 
-fn (b: ^button) gspan*(): ^int {
-	return &b.span
-}
-
-fn (b: ^button) gpadding*(): ^rectangle.rect {
-	return &b.padding
-}
-
-fn (b: ^button) gdimensions*(): ^rectangle.rect {
-	return &b.dimensions
+fn (b: ^button) get*(): ^generic {
+	return &b.g
 }
 
 fn (b: ^button) handle*() {
 	mx, my := input.getmousepos()
 	mp := input.ispressed(input.LMB)
 
-	ms := (b.dimensions.x <= mx && b.dimensions.y <= my && mx <= b.dimensions.x + b.dimensions.w && my <= b.dimensions.y + b.dimensions.h)
-	//printf("%d, %d, %d, %d\n", b.dimensions.x, b.dimensions.y, b.dimensions.x + b.dimensions.w, b.dimensions.y + b.dimensions.h)
-	//printf("%d, %d, %d, %d\n", mx, my, int(mp), int(ms))
+	ms := (
+		b.g.dimensions.x <= mx &&
+		b.g.dimensions.y <= my &&
+		mx <= b.g.dimensions.x + b.g.dimensions.w &&
+		my <= b.g.dimensions.y + b.g.dimensions.h)
 
 	b.justpressed = false
 	b.justreleased = false 
@@ -327,19 +323,18 @@ fn (b: ^button) handle*() {
 		b.toggled = !b.toggled
 		b.ontoggle(b.toggled)
 	}
-
-	//printf("%d\n", int(b.pressed))
 }
 
 ////////////////////////////////
 // main handler
+// doing it like this is stupid.
 
 var mainelement*: element
 
 // no need to call this
-fn update*(w, h: int32) {
-	dims := mainelement.gdimensions()
-	padd := mainelement.gpadding()
+fn update(w, h: int32) {
+	dims := &mainelement.get().dimensions
+	padd := &mainelement.get().padding
 	dims.x = padd.x
 	dims.y = padd.y
 	dims.w = w - padd.w - padd.x
@@ -347,7 +342,7 @@ fn update*(w, h: int32) {
 	mainelement.calculate()
 }
 
-// call this every loop
+// Call this every loop.
 fn handle*(w, h: int32) {
 	if tophat.t % 100 < 32 {
 		update(w, h)