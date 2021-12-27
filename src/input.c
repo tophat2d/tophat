de36daf7925a544fd0ca35934e15e948077c13f4
Marek Maskarinec <marek@mrms.cz>
2021-12-27 15:11:34 +0100

    convert linux keycodes to windows ones

--- src/input.c
+++ src/input.c
@@ -3,34 +3,58 @@
 
 extern th_global thg;
 
+#define LINUX_KEY_SHIFT 65505
+#define LINUX_KEY_BACKSPACE 65288
+#define LINUX_KEY_DELETE 65535
+#define LINUX_KEY_LEFT_ARROW 65361
+#define LINUX_KEY_RIGHT_ARROW 65363
+#define LINUX_KEY_TOP_ARROW 65362
+#define LINUX_KEY_BOTTOM_ARROW 65364
+#define LINUX_KEY_ESCAPE 65307
+#define LINUX_KEY_ENTER 65293
+
+#define WIN_KEY_SHIFT 16
+#define WIN_KEY_BACKSPACE 8
+#define WIN_KEY_DELETE 46
+#define WIN_KEY_LEFT_ARROW 37
+#define WIN_KEY_RIGHT_ARROW 39
+#define WIN_KEY_TOP_ARROW 38
+#define WIN_KEY_BOTTOM_ARROW 40
+#define WIN_KEY_ESCAPE 27
+#define WIN_KEY_ENTER 13
+
 void HandleKey(int keycode, int bDown) {
-	if (keycode > 255) {
-		switch (keycode) {
-		case 65288:
-			keycode = 0;
-			break;
-		case 65307:
-			keycode = 1;
-			break;
-		case 65293:
-			keycode = 2;
-			break;
-		case 65362:
-			keycode = 3;
-			break;
-		case 65364:
-			keycode = 4;
-			break;
-		case 65361:
-			keycode = 5;
-			break;
-		case 65363:
-			keycode = 6;
-			break;
-		default:
-			return;
-		}
+#ifdef __linux__
+	switch (keycode) {
+	case LINUX_KEY_SHIFT:
+		keycode = WIN_KEY_SHIFT;
+		break;
+	case LINUX_KEY_BACKSPACE:
+		keycode = WIN_KEY_BACKSPACE;
+		break;
+	case LINUX_KEY_DELETE:
+		keycode = WIN_KEY_DELETE;
+		break;
+	case LINUX_KEY_LEFT_ARROW:
+		keycode = WIN_KEY_LEFT_ARROW;
+		break;
+	case LINUX_KEY_RIGHT_ARROW:
+		keycode = WIN_KEY_RIGHT_ARROW;
+		break;
+	case LINUX_KEY_TOP_ARROW:
+		keycode = WIN_KEY_TOP_ARROW;
+		break;
+	case LINUX_KEY_BOTTOM_ARROW:
+		keycode = WIN_KEY_BOTTOM_ARROW;
+		break;
+	case LINUX_KEY_ESCAPE:
+		keycode = WIN_KEY_ESCAPE;
+		break;
+	case LINUX_KEY_ENTER:
+		keycode = WIN_KEY_ENTER;
+		break;
 	}
+#endif
 
 	if (!bDown) {
 		thg.pressed[keycode] = 0;
@@ -47,7 +71,7 @@ void HandleKey(int keycode, int bDown) {
 	thg.just_pressed[keycode] = 0;
 }
 void HandleButton( int x, int y, int button, int bDown ) {
-	HandleKey(button+6, bDown);
+	HandleKey(button, bDown);
 }
 
 void HandleMotion( int x, int y, int mask ) {
