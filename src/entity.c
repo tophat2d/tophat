dadcec8a5eba60a55dfe718341451515c609e578
Marek Maskarinec <marek@mrms.cz>
2022-09-30 17:13:16 +0200

    cleanup

--- src/entity.c
+++ src/entity.c
@@ -18,19 +18,6 @@ void th_ent_draw(th_ent *o, th_rect *camera) {
 	th_transform t = o->t;
 	t.pos.x -= camx;
 	t.pos.y -= camy;
-
-	th_quad q;
-	if (o->img) {
-		q = o->img->crop;
-		for (int i=0; i < 4; i++) {
-			q.v[i].x *= o->img->dm.x;
-			q.v[i].y *= o->img->dm.y;
-		}
-
-		th_transform_quad(&q, t);
-	} else {
-		th_transform_rect(&q, t, o->rect);
-	}
 	
 	// this logic is incorrect
 	/*if (q.br.x < 0 || q.br.y < 0)
@@ -40,6 +27,8 @@ void th_ent_draw(th_ent *o, th_rect *camera) {
 		return;*/
 
 	if (!o->img) {
+		th_quad q;
+		th_transform_rect(&q, t, o->rect);
 		for (uu i=0; i < 4; i++) {
 			q.v[i].x *= thg->scaling;
 			q.v[i].y *= thg->scaling;
