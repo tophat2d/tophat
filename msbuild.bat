adb44c3d2f95eca97221373ce0c620acbc666796
skejeton <ishidex2@gmail.com>
2022-10-26 21:23:56 +0600

    windows build: improve build scripts

--- msbuild.bat
+++ msbuild.bat
@@ -1,15 +1,16 @@
+@echo off
 rem Detect if VC devenv is activated by checking if cl is available
 where cl
 if %ERRORLEVEL% NEQ 0 (
-    echo "Error: You need to enable VC developer console. Press the windows key and type 'Developer Command Prompt for VS'"    
-    echo "       and run the command again."    
-    exit -1
+    echo Error: You need to enable VC developer console. Press the windows key and type 'Developer Command Prompt for VS'
+    echo        and run the command again.
+    exit /b 1
 )
 
 where cmake
 if %ERRORLEVEL% NEQ 0 (
-    echo "Error: You need cmake."    
-    exit -1
+    echo Error: You need cmake.    
+    exit /b
 )
 
 md bin
