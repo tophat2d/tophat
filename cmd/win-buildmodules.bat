ccac3320fbdf78e5cbd87a00bba7b050bd3b256f
ske <skejeton@hotmail.com>
2023-03-05 19:38:34 +0600

    add nav.um in windows build script

--- cmd/win-buildmodules.bat
+++ cmd/win-buildmodules.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set modules=umka/anim.um umka/audio.um umka/csv.um umka/ent.um umka/image.um umka/input.um umka/misc.um umka/canvas.um umka/ray.um umka/rect.um umka/tilemap.um umka/window.um umka/ui.um umka/std/std.um umka/particles.um umka/lerp.um umka/utf8.um umka/font.um umka/th.um umka/signal.um umka/atlas.um umka/ui/label.um umka/ui/grid.um umka/ui/imagebox.um umka/shader.um umka/color.um umka/coll.um umka/placeholders.um
+set modules=umka/anim.um umka/audio.um umka/csv.um umka/ent.um umka/image.um umka/input.um umka/misc.um umka/canvas.um umka/ray.um umka/rect.um umka/tilemap.um umka/window.um umka/ui.um umka/std/std.um umka/particles.um umka/lerp.um umka/utf8.um umka/font.um umka/th.um umka/signal.um umka/atlas.um umka/ui/label.um umka/ui/grid.um umka/ui/imagebox.um umka/shader.um umka/color.um umka/coll.um umka/placeholders.um umka/nav.um
 set module_names=%modules:umka/=%
 set umka=lib\umka\umka_windows_msvc\umka.exe
 set em_target=src\staembed.c
