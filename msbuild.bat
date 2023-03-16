a9c665718c8de6c7075048f5209c49a386f81f3d
ske <skejeton@hotmail.com>
2023-03-16 22:51:19 +0600

    fix windows build

--- msbuild.bat
+++ msbuild.bat
@@ -2,7 +2,7 @@
 rem Detect if VC devenv is activated by checking if cl is available
 where cl
 if %ERRORLEVEL% NEQ 0 (
-    echo Error: You need to enable VC developer console. Press the windows key and type 'Developer Command Prompt for VS'
+    echo Error: You need to enable VC developer console. Press the windows key and type 'x64 Native Tools Command Prompt for VS'
     echo        and run the command again.
     exit /b 1
 )
@@ -13,8 +13,11 @@ if %ERRORLEVEL% NEQ 0 (
     exit /b
 )
 
+set target=Release
+
 md bin
 cd bin
-cmake -DCMAKE_BUILD_TYPE=Release .. && msbuild tophat.sln /property:Configuration=Release
+cmake -DCMAKE_BUILD_TYPE=%target% .. && msbuild tophat.sln /property:Configuration=%target%
 cd ..
-copy bin\Release\tophat.exe tophat.exe
+copy bin\%target%\tophat.exe tophat.exe
+
