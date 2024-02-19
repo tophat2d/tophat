44148c97c6e2a89b50374f339c4b162685945713
Marek Maskarinec <marek@mrms.cz>
2024-02-19 09:56:17 -0800

    Add testing to CI (#145)
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

---
+++ cmd/checkall.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+# check main.um
+./tophat -check
+
+for f in tests/*.um; do
+	./tophat -check -main $f
+done
+
+cd examples/extensions
+../../tophat -check
+
+cd ../flappy
+../../tophat -check
+
+cd ../hello-world
+../../tophat -check
+
+cd ../platformer
+../../tophat -check
+
+cd ../pomodoro
+../../tophat -check
+
+cd ../space-shooter
+../../tophat -check
+
+cd ../tetris
+../../tophat -check
