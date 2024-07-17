ad9cda3d5cd0245e263e2d6d674ac03095e0a1bd
Marek Maškarinec <marek@mrms.cz>
2024-07-17 22:30:18 +0200

    Update cmd/checkall.sh
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

--- cmd/checkall.sh
+++ cmd/checkall.sh
@@ -4,26 +4,34 @@
 ./tophat -check
 
 for f in tests/*.um; do
+	echo CH $f
 	./tophat -check -main $f
 done
 
+echo CH demos/extensions
 cd demos/extensions
 ../../tophat -check
 
+echo CH demos/flappy
 cd ../flappy
 ../../tophat -check
 
+echo CH demos/hello-world
 cd ../hello-world
 ../../tophat -check
 
+echo CH demos/platformer
 cd ../platformer
 ../../tophat -check
 
+echo CH demos/pomodoro
 cd ../pomodoro
 ../../tophat -check
 
+echo CH demos/space-shooter
 cd ../space-shooter
 ../../tophat -check
 
+echo CH demos/tetris
 cd ../tetris
 ../../tophat -check
