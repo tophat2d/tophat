e28a451d39ea8b8b858a68d5f7e4a34eaef189c2
Vasiliy Tereshkov <vtereshkov@mail.ru>
2026-04-13 02:12:36 +0400

    Rename folder

--- build_android.bat
+++ build_android.bat
@@ -1,7 +1,7 @@
-copy demos\flappy\*.ttf gradle\app\src\main\assets\tophat
-copy demos\flappy\gfx\*.* gradle\app\src\main\assets\tophat
-copy demos\flappy\sfx\*.* gradle\app\src\main\assets\tophat
+copy demos\flappy\*.ttf android\app\src\main\assets\tophat
+copy demos\flappy\gfx\*.* android\app\src\main\assets\tophat
+copy demos\flappy\sfx\*.* android\app\src\main\assets\tophat
 
 set ANDROID_HOME=%LOCALAPPDATA%\Android\Sdk
-cd gradle
+cd android
 gradlew assembleDebug
