49e0306a97aeb60564287ac6fa6b40e5cafe2476
Vasiliy Tereshkov <vtereshkov@mail.ru>
2026-04-13 01:22:27 +0400

    Remove unnecessary files

--- gradle/app/build.gradle.kts
+++ gradle/app/build.gradle.kts
@@ -25,10 +25,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
