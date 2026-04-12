3a84b78f998f73b0ba4c1b12b7d0b680dfee75c0
Vasiliy Tereshkov <vtereshkov@mail.ru>
2026-04-12 21:34:26 +0400

    Experimental Android support

--- src/misc.c
+++ src/misc.c
@@ -11,6 +11,10 @@
 #include <unistd.h>
 #endif
 
+#ifdef __ANDROID__
+#include <android/log.h>
+#endif
+
 #define MSG_LEN 1024
 
 extern th_global *thg;
@@ -48,6 +52,8 @@ th_error(char *text, ...)
 {
 #ifdef __EMSCRIPTEN__
 	fprintf(stderr, "error: ");
+#elif defined(__ANDROID__)
+	// No prefix
 #else
 	if (isatty(fileno(stderr))) {
 		fprintf(stderr, "\x1b[1m\x1b[31merror: \x1b[0m");
@@ -66,8 +72,12 @@ th_error(char *text, ...)
 		MessageBox(NULL, buf, "tophat error", 0x10);
 	}
 #endif
+#ifdef __ANDROID__
+    __android_log_vprint(ANDROID_LOG_ERROR, "tophat", text, args);
+#else
 	vfprintf(stderr, text, args);
 	fprintf(stderr, "\n");
+#endif
 	va_end(args);
 }
 
@@ -84,8 +94,13 @@ th_info(char *text, ...)
 		MessageBox(NULL, buf, "information", 0x40);
 	}
 #endif
+#ifdef __ANDROID__
+    __android_log_vprint(ANDROID_LOG_INFO, "tophat", text, args);
+#else
 	vfprintf(stderr, text, args);
 	fprintf(stderr, "\n");
+    fflush(stderr);
+#endif
 	va_end(args);
 }
 
@@ -189,7 +204,11 @@ th_print_umka_error_and_quit(int code)
 	if (error->code != 0 && error->msg[0]) {
 		th_error("%s (%d): %s\n", error->fileName, error->line, error->msg);
 
+#ifdef __ANDROID__
+		__android_log_print(ANDROID_LOG_ERROR, "tophat", "\tStack trace:");
+#else
 		fprintf(stderr, "\tStack trace:\n");
+#endif
 
 		for (int depth = 0; depth < 10; depth++) {
 			char fnName[MSG_LEN + 1];
@@ -198,9 +217,16 @@ th_print_umka_error_and_quit(int code)
 
 			if (!umkaGetCallStack(
 				thg->umka, depth, MSG_LEN + 1, &offset, file, fnName, &line)) {
+#ifdef __ANDROID__
+				__android_log_print(ANDROID_LOG_ERROR, "tophat", "\t\t...");
+#else
 				fprintf(stderr, "\t\t...\n");
+#endif
 				break;
 			}
+#ifdef __ANDROID__
+			__android_log_print(ANDROID_LOG_ERROR, "tophat", "\t\t%s:%06d: %s", file, line, fnName);
+#else
 			if (isatty(fileno(stderr))) {
 				fprintf(stderr, "\033[34m");
 			}
@@ -209,6 +235,7 @@ th_print_umka_error_and_quit(int code)
 				fprintf(stderr, "\033[0m");
 			}
 			fprintf(stderr, "%s\n", fnName);
+#endif
 		}
 	}
 
